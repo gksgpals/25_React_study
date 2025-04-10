4521707e916762b45b6100e6ff41059cc8c79b3d
한혜민 <hanhyemin@hanhyemin-ui-MacBookAir.local>
2025-04-10 12:17:07 +0900

    리액트 프로젝트 업로드

---
+++ ProfileCard.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function ProfileCard({ name, age, hobby }) {
+  const [food, setFood] = useState("알리오올리오");
+
+  const onChangeFood = () => {
+    setFood(food === "알리오올리오" ? "순두부찌개" : "알리오올리오");
+  };
+
+  return (
+    <div style={{ border: "1px solid #ddd", padding: "16px", width: "300px" }}>
+      <h2>자기소개 카드</h2>
+      <p><b>이름:</b> {name}</p>
+      <p><b>나이:</b> {age}</p>
+      <p><b>취미:</b> {hobby}</p>
+      <p><b>좋아하는 음식:</b> {food}</p>
+      <button onClick={onChangeFood}>음식 바꾸기</button>
+    </div>
+  );
+}
+
+export default ProfileCard;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileCard from "./ProfileCard";
+
+function App() {
+  return (
+    <div>
+      <ProfileCard name="한혜민" age={24} hobby="아무 것도 안하기" />
+    </div>
+  );
+}
+
+export default App;
